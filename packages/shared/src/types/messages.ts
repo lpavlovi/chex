4c332aecf5a8d05c38a5a056e6bfa44de5e9ae50
Luka Pavlovic <pavlovic224@gmail.com>
2025-11-12 17:10:12 -0500

    update the shared messages

--- packages/shared/src/types/messages.ts
+++ packages/shared/src/types/messages.ts
@@ -1,17 +1,24 @@
 // Message type definitions for Chrome extension communication
 
 export type EchoMessage = {
-  readonly type: "echo";
-  readonly message: string;
+	readonly type: "echo";
+	readonly message: string;
+};
+
+export type UserInfoRequestMessage = {
+	readonly type: "userInfo";
 };
 
 export type LoginMessage = {
-  readonly type: "login";
-  readonly token: string;
+	readonly type: "login";
 };
 
 export type LogoutMessage = {
-  readonly type: "logout";
+	readonly type: "logout";
 };
 
-export type ExtensionMessage = EchoMessage | LoginMessage | LogoutMessage;
+export type ExtensionMessage =
+	| EchoMessage
+	| LoginMessage
+	| LogoutMessage
+	| UserInfoRequestMessage;
