d1301251e62096571d7b02d2263f6a56ef8981cf
Luka Pavlovic <pavlovic224@gmail.com>
2025-12-16 14:56:36 +0100

    app state store redesign

--- packages/extension/src/content/context/state/entity.ts
+++ packages/extension/src/content/context/state/entity.ts
@@ -1,15 +1,15 @@
 import { createContext } from "solid-js";
-import type { Accessor, Setter } from "solid-js";
+import type { Store, SetStoreFunction } from "solid-js/store";
 
 type StateOption = "idle" | "listening";
-export type AppState = {current: StateOption};
+export type AppState = { current: StateOption };
 
-export type AppStateContextType = [Accessor<AppState>, Setter<AppState>];
+export type AppStateContextType = [Store<AppState>, SetStoreFunction<AppState>];
 
-export const DEFAULT_APP_STATE: AppState = {current: "idle"};
+export const DEFAULT_APP_STATE: AppState = { current: "idle" };
 
 export const AppStateContext = createContext<AppStateContextType>([
-  () => DEFAULT_APP_STATE,
-  (_v: AppState | ((prev: AppState) => AppState)) => {},
+  DEFAULT_APP_STATE as Store<AppState>,
+  (() => {}) as SetStoreFunction<AppState>,
 ]);
 
--- packages/extension/src/content/context/state/provider.tsx
+++ packages/extension/src/content/context/state/provider.tsx
@@ -1,11 +1,11 @@
-import { createSignal } from "solid-js";
-import type { JSX } from "solid-js";
+import { createStore } from "solid-js/store";
 import { AppStateContext, DEFAULT_APP_STATE } from "./entity";
+import type { JSX } from "solid-js";
 import type { AppStateContextType } from "./entity";
 
 export function StateProvider(props: { children?: JSX.Element }) {
-  const [value, setValue] = createSignal(DEFAULT_APP_STATE);
-  const appState: AppStateContextType = [value, setValue];
+  const [state, setState] = createStore(DEFAULT_APP_STATE);
+  const appState: AppStateContextType = [state, setState];
   return (
     <AppStateContext.Provider value={appState}>
       {props.children}
